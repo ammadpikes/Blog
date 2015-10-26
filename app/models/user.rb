24bed875ac29abf0ac5991e92731214e31c9a42d
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-26 17:13:02 +0500

    User Styled

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Upper Mentioed all Functions are available in this application. That's why declared like that. 
 end
