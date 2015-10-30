0e11e58949385f9db69a81e5231c8d4211693791
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-30 16:09:18 +0500

    Confirmation Email Done - Messages Flashes Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   # Upper Mentioed all Functions are available in this application. That's why declared like that.
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url:  "http://www.sinaiem.org/people/files/2013/03/missing.png"
